a49eb99ddd6746abb95c71c987297c32fe94a30b
EJ <evyatar.shafran@gmail.com>
2026-01-14 23:21:22 +0200

    Undo temporary changes

--- build.c
+++ build.c
@@ -125,37 +125,6 @@ bool build_test(Arena *arena, const char *path, FilePaths dependencies,
 
     Command command = command_new(lt.arena, 32);
 
-#ifdef _WIN32
-    COMMAND_CC(&command);
-
-    COMMAND_CC_FLAGS(&command);
-
-    COMMAND_CC_DEFINE(lt.arena, &command, "BOOKSTORE_IMPLEMENTATION");
-
-    COMMAND_APPEND(&command, "/EP");
-
-    COMMAND_CC_INPUTS(&command, path);
-
-    StringBuilder processed =
-        sb_new(lt.arena, basename.count + output_dir.count + 4);
-    sb_appendf(&processed, SV_FMT SYSTEM_PATH_DELIMITER_STRING SV_FMT ".c",
-               SV_ARG(output_dir), SV_ARG(basename));
-    sb_push_null(&processed);
-
-    if (!COMMAND_RUN(arena, &command, .stdout_path = processed.items))
-        DEFER_RETURN(false);
-
-    path = processed.items;
-
-    StringView sv = read_entire_file(lt.arena, processed.items);
-    i32 i = 0;
-    while (sv.count) {
-        i++;
-        StringView line = sv_cut_delimiter(&sv, '\n');
-        printf("%d " SV_FMT "\n", i, SV_ARG(line));
-    }
-#endif // _WIN32
-
     COMMAND_CC(&command);
 
     COMMAND_CC_FLAGS(&command);
@@ -168,7 +137,9 @@ bool build_test(Arena *arena, const char *path, FilePaths dependencies,
     COMMAND_CC_OUTPUT(lt.arena, &command, output.items);
     COMMAND_CC_INPUTS(&command, path);
 
-    if (!COMMAND_RUN(lt.arena, &command)) DEFER_RETURN(false);
+    if (!COMMAND_RUN(lt.arena, &command, .async = procs,
+                     .concurrency = concurrency))
+        DEFER_RETURN(false);
 
     DEFER_LABEL({ lifetime_end(lt); });
 }
