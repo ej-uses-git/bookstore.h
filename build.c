3f358aa266b5d9eb845e31b8a45ad8ee0bebae97
EJ <evyatar.shafran@gmail.com>
2026-01-14 14:07:14 +0200

    Run tests by iterating inputs

--- build.c
+++ build.c
@@ -3,6 +3,7 @@
 #include "./bookstore/build.h"
 #include "./bookstore/arena.h"
 #include "./bookstore/basic.h"
+#include "./bookstore/command.h"
 #include "./bookstore/flag.h"
 #include "./bookstore/string.h"
 #include "./bookstore/system.h"
@@ -12,8 +13,8 @@
 #define TEST_OUTPUT_DIR BIN_DIR SYSTEM_PATH_DELIMITER_STRING "test"
 #define TEST_INPUT_DIR  "test"
 
-bool build_tests(Arena *arena, FilePaths dependencies);
-bool run_tests(Arena *arena);
+bool build_tests(Arena *arena, FilePaths tests, FilePaths dependencies);
+bool run_tests(Arena *arena, FilePaths tests);
 
 void usage(FILE *stream) {
     // clang-format off
@@ -79,125 +80,120 @@ int main(int argc, const char **argv) {
     if (args_index_of(args, sv_from_cstr("clean")) >= 0) {
         if (!delete_directory_recursively(arena, BIN_DIR)) return 1;
     }
+
+    FilePaths tests = file_paths_new(arena, 64);
+    list_directory(arena, TEST_INPUT_DIR, &tests);
+
     if (args_index_of(args, sv_from_cstr("build")) >= 0) {
-        if (!build_tests(arena, dependencies)) return 1;
+        if (!build_tests(arena, tests, dependencies)) return 1;
     }
     if (args_index_of(args, sv_from_cstr("test")) >= 0) {
-        if (!run_tests(arena)) return 1;
+        if (!run_tests(arena, tests)) return 1;
     }
 
     return 0;
 }
 
-typedef struct {
-    ProcessList *procs;
-    FilePaths dependencies;
-    i32 concurrency;
-} BuildTestsVisitData;
-
-bool build_tests_visit(WalkEntry entry) {
+bool build_test(Arena *arena, const char *path, FilePaths dependencies,
+                ProcessList *procs, i32 concurrency) {
     DEFER_SETUP(bool, true);
 
-    Lifetime lt = lifetime_begin(entry.arena);
+    Lifetime lt = lifetime_begin(arena);
 
-    if (entry.level > 1) {
-        *entry.action = WALK_STOP;
+    FilePaths all_dependencies =
+        file_paths_new(lt.arena, dependencies.count + 1);
+    file_paths_push(&all_dependencies, path);
+    file_paths_append_other(&all_dependencies, dependencies);
+
+    StringView output_dir = sv_from_cstr(TEST_OUTPUT_DIR);
+
+    StringView basename = get_basename(sv_from_cstr(path));
+    sv_strip_suffix(&basename, sv_from_cstr(".c"));
+
+    StringBuilder output =
+        sb_new(lt.arena, basename.count + output_dir.count + 2);
+    sb_appendf(&output, SV_FMT SYSTEM_PATH_DELIMITER_STRING SV_FMT,
+               SV_ARG(output_dir), SV_ARG(basename));
+    sb_push_null(&output);
+
+    i8 needs_rebuild = build_needs_rebuild(output.items, all_dependencies);
+    if (needs_rebuild < 0) DEFER_RETURN(false);
+    if (!needs_rebuild) {
+        log_debug("Nothing to do for '%s'", output.items);
         DEFER_RETURN(true);
     }
 
-    if (entry.level == 1) {
-        BuildTestsVisitData *data = entry.user_data;
+    Command command = command_new(lt.arena, 32);
 
-        StringView output_dir = sv_from_cstr(TEST_OUTPUT_DIR);
+    COMMAND_CC(&command);
 
-        FilePaths dependencies =
-            file_paths_new(lt.arena, data->dependencies.count + 1);
-        file_paths_push(&dependencies, entry.path);
-        file_paths_append_other(&dependencies, data->dependencies);
+    COMMAND_CC_FLAGS(&command);
 
-        StringView basename = get_basename(sv_from_cstr(entry.path));
-        sv_strip_suffix(&basename, sv_from_cstr(".c"));
+    // TODO: do this only in debug mode?
+    COMMAND_CC_DEBUG_INFO(&command);
+    COMMAND_CC_ADDRESS_SANITIZE(&command);
 
-        StringBuilder output =
-            sb_new(lt.arena, basename.count + output_dir.count + 2);
-        sb_appendf(&output, SV_FMT SYSTEM_PATH_DELIMITER_STRING SV_FMT,
-                   SV_ARG(output_dir), SV_ARG(basename));
-        sb_push_null(&output);
+    COMMAND_CC_DEFINE(lt.arena, &command, "BOOKSTORE_IMPLEMENTATION");
+    COMMAND_CC_OUTPUT(lt.arena, &command, output.items);
+    COMMAND_CC_INPUTS(&command, path);
 
-        i8 needs_rebuild = build_needs_rebuild(output.items, dependencies);
-        if (needs_rebuild < 0) DEFER_RETURN(false);
-        if (!needs_rebuild) {
-            log_debug("Nothing to do for '%s'", output.items);
-            DEFER_RETURN(true);
-        }
-
-        Command command = command_new(lt.arena, 32);
-
-        COMMAND_CC(&command);
-
-        COMMAND_CC_FLAGS(&command);
-
-        // TODO: do this only in debug mode?
-        COMMAND_CC_DEBUG_INFO(&command);
-        COMMAND_CC_ADDRESS_SANITIZE(&command);
-
-        COMMAND_CC_DEFINE(lt.arena, &command, "BOOKSTORE_IMPLEMENTATION");
-        COMMAND_CC_OUTPUT(lt.arena, &command, output.items);
-        COMMAND_CC_INPUTS(&command, entry.path);
-
-        if (!COMMAND_RUN(lt.arena, &command, .async = data->procs,
-                         .concurrency = data->concurrency))
-            DEFER_RETURN(false);
-    }
+    if (!COMMAND_RUN(lt.arena, &command, .async = procs,
+                     .concurrency = concurrency))
+        DEFER_RETURN(false);
 
     DEFER_LABEL({ lifetime_end(lt); });
 }
 
-bool build_tests(Arena *arena, FilePaths dependencies) {
+bool build_tests(Arena *arena, FilePaths tests, FilePaths dependencies) {
+    if (!make_directory_recursively(arena, TEST_OUTPUT_DIR)) return false;
+
     i32 concurrency = 64;
     ProcessList procs = process_list_new(arena, concurrency);
 
-    BuildTestsVisitData data = {.procs = &procs,
-                                .dependencies = dependencies,
-                                .concurrency = concurrency};
-
-    if (!make_directory_recursively(arena, TEST_OUTPUT_DIR)) return false;
-
-    if (!WALK_DIRECTORY(arena, TEST_INPUT_DIR, build_tests_visit,
-                        .user_data = &data)) {
-        return false;
+    for (i32 i = 0; i < tests.count; i++) {
+        if (!build_test(arena, file_paths_get(tests, i), dependencies, &procs,
+                        concurrency))
+            return false;
     }
 
-    if (!process_list_wait(procs)) return false;
-
-    return true;
+    return process_list_wait(procs);
 }
 
-bool run_tests_visit(WalkEntry entry) {
+bool run_test(Arena *arena, const char *path) {
     DEFER_SETUP(bool, true);
 
-    Lifetime lt = lifetime_begin(entry.arena);
+    Lifetime lt = lifetime_begin(arena);
 
-    if (entry.level > 1) {
-        *entry.action = WALK_SKIP;
-        DEFER_RETURN(true);
-    }
+    StringView output_dir = sv_from_cstr(TEST_OUTPUT_DIR);
 
-    if (entry.level == 1) {
-        if (entry.type != FILE_TYPE_REGULAR ||
-            sv_ends_with(sv_from_cstr(entry.path), sv_from_cstr(".obj"))) {
-            DEFER_RETURN(true);
-        }
+    StringView basename = get_basename(sv_from_cstr(path));
+    sv_strip_suffix(&basename, sv_from_cstr(".c"));
 
-        Command command = command_new(lt.arena, 1);
-        command_push(&command, entry.path);
+#ifdef _WIN32
+    i32 executable_length = basename.count + output_dir.count + 6;
+#else
+    i32 executable_length = basename.count + output_dir.count + 2;
+#endif // _WIN32
 
-        if (!COMMAND_RUN(lt.arena, &command)) DEFER_RETURN(false);
-    }
+    StringBuilder executable = sb_new(lt.arena, executable_length);
+    sb_appendf(&executable, SV_FMT SYSTEM_PATH_DELIMITER_STRING SV_FMT,
+               SV_ARG(output_dir), SV_ARG(basename));
+#ifdef _WIN32
+    sb_append_cstr(&executable, ".exe");
+#endif // _WIN32
+    sb_push_null(&executable);
+
+    Command command = command_new(lt.arena, 1);
+    command_push(&command, executable.items);
+
+    if (!COMMAND_RUN(lt.arena, &command)) DEFER_RETURN(false);
 
     DEFER_LABEL({ lifetime_end(lt); });
 }
 
-bool run_tests(Arena *arena) {
-    return WALK_DIRECTORY(arena, TEST_OUTPUT_DIR, run_tests_visit);
+bool run_tests(Arena *arena, FilePaths tests) {
+    for (i32 i = 0; i < tests.count; i++) {
+        if (!run_test(arena, file_paths_get(tests, i))) return false;
+    }
+    return true;
 }
