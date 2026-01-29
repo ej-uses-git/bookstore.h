08a656cb45837198da1c93f0f31bc886e6efdc14
EJ <evyatar.shafran@gmail.com>
2026-01-29 14:02:07 +0200

    [arena] Use properties for testing

--- test/arena.c
+++ test/arena.c
@@ -3,63 +3,61 @@
 #include "../bookstore/arena.h"
 
 TEST_MAIN({
-    i32 size = 4 * sizeof(i32);
     Arena *arena = NULL;
 
-    BEFORE_EACH({
-        // Recreate the arena
-        arena = arena_new(size);
-    });
     AFTER_EACH({
         // Free the underlying memory
         if (arena) arena_destroy(arena);
     });
 
     DESCRIBE("arena_alloc", {
-        IT("should allocate memory from the arena", {
-            i32 *buf = arena_alloc(arena, size);
-            buf[0] = 0;
-            buf[1] = 1;
-            buf[2] = 2;
-            buf[3] = 3;
+        PROP("should allocate memory from the arena", rng, {
+            i32 size = random_next_i32_ranged(&rng, 0, KiB(2));
+            arena = arena_new(size * sizeof(i32));
+
+            i32 *buf = arena_alloc(arena, size * sizeof(i32));
+            for (i32 i = 0; i < size; i++) buf[i] = i;
         });
 
-        IT_FAIL("asserts that the capacity isn't bypassed",
-                { arena_alloc(arena, size + 1); });
+        PROP_FAIL("asserts that the capacity isn't bypassed", rng, {
+            i32 size = random_next_i32_ranged(&rng, 0, INT32_MAX);
+            arena = arena_new(size);
+            arena_alloc(arena, size + 1);
+        });
     });
 
     DESCRIBE("arena_clear", {
-        IT("should reset the arena's allocations", {
-            i32 *buf = arena_alloc(arena, size);
-            buf[0] = 0;
-            buf[1] = 1;
-            buf[2] = 2;
-            buf[3] = 3;
+        PROP("should reset the arena's allocations", rng, {
+            i32 size = random_next_i32_ranged(&rng, 0, KiB(2));
+            arena = arena_new(size * sizeof(i32));
+
+            i32 *buf = arena_alloc(arena, size * sizeof(i32));
+            for (i32 i = 0; i < size; i++) buf[i] = i;
 
             arena_clear(arena);
-            EXPECT_EQ(arena->allocated, 0, "%d");
+            EXPECT_EQ(arena->allocated, 0, I32_FMT);
 
-            buf = arena_alloc(arena, size);
-            buf[0] = 0;
-            buf[1] = 1;
-            buf[2] = 2;
-            buf[3] = 3;
+            buf = arena_alloc(arena, size * sizeof(i32));
+            for (i32 i = 0; i < size; i++) buf[i] = i;
         });
     });
 
     DESCRIBE("lifetime_begin/lifetime_end", {
-        IT("should provide users with a temporary lifetime to allocate with", {
+        PROP("should create/end a temporary allocation lifetime", rng, {
+            i32 size = random_next_i32_ranged(&rng, 0, KiB(2));
+            arena = arena_new(size * sizeof(i32));
+
             arena_alloc(arena, 1);
 
             Lifetime lt = lifetime_begin(arena);
 
             arena_alloc(lt.arena, size - 1);
 
-            EXPECT_EQ(arena->allocated, size, "%d");
+            EXPECT_EQ(arena->allocated, size, I32_FMT);
 
             lifetime_end(lt);
 
-            EXPECT_EQ(arena->allocated, 1, "%d");
+            EXPECT_EQ(arena->allocated, 1, I32_FMT);
         });
     });
 })
